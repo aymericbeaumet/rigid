8df0dd679a238e0a42db97358b34244da5fc7e0e
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-15 23:35:39 +0200

    dynamic steps generation

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,50 @@ use proc_macro::TokenStream;
 #[proc_macro_derive(JSONParser)]
 pub fn derive_json_parser(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let typename = input.ident;
+
     let data = match input.data {
         syn::Data::Struct(s) => s,
         _ => {
             return TokenStream::default();
         }
     };
-    let typename = input.ident;
+
+    let mut steps = vec![];
+
+    steps.push(quote::quote! {
+                idx += eat_whitespaces(&bytes[idx..])?;
+                idx += eat_char(&bytes[idx..], b'{')?;
+                idx += eat_whitespaces(&bytes[idx..])?;
+    });
+
+    steps.push(quote::quote! {
+                let (delta, age) = eat_object_key_value(&bytes[idx..], "age".as_bytes())?;
+                idx += delta;
+    });
+
+    steps.push(quote::quote! {
+                idx += eat_whitespaces(&bytes[idx..])?;
+                idx += eat_char(&bytes[idx..], b'}')?;
+                idx += eat_whitespaces(&bytes[idx..])?;
+    });
 
     let from_json_str_impl = quote::quote! {
+        impl #typename {
+            fn from_json_str(s: &str) -> Result<#typename, ()> {
+                let bytes = s.as_bytes();
+                let mut idx = 0;
+
+                #(#steps)*
+
+                if idx == s.len() {
+                    Ok(#typename { age: age })
+                } else {
+                    Err(())
+                }
+            }
+        }
+
         fn eat_whitespaces(bytes: &[u8]) -> Result<usize, ()> {
             let mut idx = 0;
             while idx < bytes.len() && bytes[idx] == b' ' {
@@ -68,30 +103,6 @@ pub fn derive_json_parser(input: TokenStream) -> TokenStream {
                 Ok(s.len())
             } else {
                 Err(())
-            }
-        }
-
-        impl #typename {
-            fn from_json_str(s: &str) -> Result<#typename, ()> {
-                let bytes = s.as_bytes();
-                let mut idx = 0;
-
-                idx += eat_whitespaces(&bytes[idx..])?;
-                idx += eat_char(&bytes[idx..], b'{')?;
-                idx += eat_whitespaces(&bytes[idx..])?;
-
-                let (delta, age) = eat_object_key_value(&bytes[idx..], "age".as_bytes())?;
-                idx += delta;
-
-                idx += eat_whitespaces(&bytes[idx..])?;
-                idx += eat_char(&bytes[idx..], b'}')?;
-                idx += eat_whitespaces(&bytes[idx..])?;
-
-                if idx == s.len() {
-                    Ok(#typename { age: age })
-                } else {
-                    Err(())
-                }
             }
         }
     };
