4e830fafac31949fa70d233ad286013e19a2ee74
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-15 18:50:56 +0200

    add test-case

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,7 +5,7 @@ struct Person {
 
 #[test]
 fn it_should_match_serde_output() {
-    for input in &[r#"{ "age": 43 }"#] {
+    for input in &[r#"{"age":21}"#, r#" { "age": 42 } "#] {
         let output = Person::json_from_str(input).unwrap();
         let serde_output: Person = serde_json::from_str(input).unwrap();
         assert_eq!(output, serde_output);
