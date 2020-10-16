bc55ed685bbe957c7eabad6b2e754a0098b4dbe5
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-16 07:18:35 +0200

    inlining the parsing helpers provide a 36% increase

--- rigid_runtime/src/lib.rs
+++ rigid_runtime/src/lib.rs
@@ -1,5 +1,6 @@
 pub struct Error(String);
 
+#[inline(always)]
 pub fn eat_whitespaces(bytes: &[u8]) -> Result<usize, String> {
     let mut idx = 0;
     while idx < bytes.len() && bytes[idx] == b' ' {
@@ -8,6 +9,7 @@ pub fn eat_whitespaces(bytes: &[u8]) -> Result<usize, String> {
     Ok(idx)
 }
 
+#[inline(always)]
 pub fn eat_char(bytes: &[u8], c: u8) -> Result<usize, String> {
     if bytes.len() >= 1 && bytes[0] == c {
         Ok(1)
@@ -19,6 +21,7 @@ pub fn eat_char(bytes: &[u8], c: u8) -> Result<usize, String> {
     }
 }
 
+#[inline(always)]
 pub fn eat_object_key_value(bytes: &[u8], k: &[u8]) -> Result<(usize, u8), String> {
     let mut idx = 0;
     idx += eat_object_key(&bytes[idx..], k)?;
@@ -32,6 +35,7 @@ pub fn eat_object_key_value(bytes: &[u8], k: &[u8]) -> Result<(usize, u8), Strin
     Ok((idx, value))
 }
 
+#[inline(always)]
 pub fn eat_object_key(bytes: &[u8], k: &[u8]) -> Result<usize, String> {
     let mut idx = 0;
     idx += eat_char(&bytes[idx..], b'"')?;
@@ -40,6 +44,7 @@ pub fn eat_object_key(bytes: &[u8], k: &[u8]) -> Result<usize, String> {
     Ok(idx)
 }
 
+#[inline(always)]
 pub fn eat_number(bytes: &[u8]) -> Result<(usize, u8), String> {
     let mut idx = 0;
     let mut out: u8 = 0;
@@ -54,6 +59,7 @@ pub fn eat_number(bytes: &[u8]) -> Result<(usize, u8), String> {
     }
 }
 
+#[inline(always)]
 pub fn eat_slice(bytes: &[u8], s: &[u8]) -> Result<usize, String> {
     if bytes.starts_with(s) {
         Ok(s.len())
