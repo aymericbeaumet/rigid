972169250393b3dc45b3cb668b4fb2d58ee9d099
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-16 09:49:22 +0200

    add comments

--- rigid/src/lib.rs
+++ rigid/src/lib.rs
@@ -1,4 +1,6 @@
+// Exposed so that the library is convenient to use for the end-user
 pub use rigid_derive::FromJSON;
-
-pub use rigid_runtime as runtime;
 pub use rigid_runtime::{Error, Result};
+
+// Exposed so that the generated code can find the runtime helpers
+pub use rigid_runtime as runtime;
