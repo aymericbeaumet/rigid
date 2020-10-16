6d42d216d4bb1a1631a1e8756f13f3eff415206f
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-16 10:25:01 +0200

    add benchmark group

--- benches/benches.rs
+++ benches/benches.rs
@@ -8,44 +8,48 @@ struct Person {
 static DATA: &str = r#"{ "age": 43 }"#;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("rigid::from_json", |b| {
+    let mut group = c.benchmark_group("Parse JSON");
+
+    group.bench_function("rigid::from_json", |b| {
         b.iter(|| {
             black_box(Person::from_json(black_box(DATA)).unwrap());
         })
     });
 
-    c.bench_function("json::parse", |b| {
+    group.bench_function("json::parse", |b| {
         b.iter(|| {
             black_box(json::parse(black_box(DATA)).unwrap());
         })
     });
 
-    c.bench_function("serde_json::from_str", |b| {
+    group.bench_function("serde_json::from_str", |b| {
         b.iter(|| {
             black_box(serde_json::from_str::<Person>(black_box(DATA)).unwrap());
         })
     });
 
-    c.bench_function("simd_json::to_borrowed_value", |b| {
+    group.bench_function("simd_json::to_borrowed_value", |b| {
         let mut data = DATA.as_bytes().to_vec();
         b.iter(|| {
             black_box(simd_json::to_borrowed_value(black_box(&mut data)).unwrap());
         })
     });
 
-    c.bench_function("simd_json::to_owned_value", |b| {
+    group.bench_function("simd_json::to_owned_value", |b| {
         let mut data = DATA.as_bytes().to_vec();
         b.iter(|| {
             black_box(simd_json::to_owned_value(black_box(&mut data)).unwrap());
         })
     });
 
-    c.bench_function("simd_json::serde::from_slice", |b| {
+    group.bench_function("simd_json::serde::from_slice", |b| {
         let mut data = DATA.as_bytes().to_vec();
         b.iter(|| {
             black_box(simd_json::serde::from_slice::<Person>(black_box(&mut data)).unwrap());
         })
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
