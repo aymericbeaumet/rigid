fea6e25eb41f026db137e06abcffa22e86d67d99
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-17 00:41:14 +0200

    update benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,6 +1,106 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 #[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
+struct StructBool {
+    height: bool,
+}
+
+fn criterion_struct_bool(c: &mut Criterion) {
+    let data: &str = r#" { "height" : true } "#;
+
+    let mut speed = c.benchmark_group("bool");
+
+    speed.bench_function("rigid::from_json", |b| {
+        b.iter(|| {
+            black_box(StructBool::from_json(black_box(data)).unwrap());
+        })
+    });
+
+    speed.bench_function("serde_json::from_str", |b| {
+        b.iter(|| {
+            black_box(serde_json::from_str::<StructBool>(black_box(data)).unwrap());
+        })
+    });
+
+    speed.finish();
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
+struct StructU8 {
+    height: u8,
+}
+
+fn criterion_struct_u8(c: &mut Criterion) {
+    let data: &str = r#" { "height" : 190 } "#;
+
+    let mut speed = c.benchmark_group("u8");
+
+    speed.bench_function("rigid::from_json", |b| {
+        b.iter(|| {
+            black_box(StructU8::from_json(black_box(data)).unwrap());
+        })
+    });
+
+    speed.bench_function("serde_json::from_str", |b| {
+        b.iter(|| {
+            black_box(serde_json::from_str::<StructU8>(black_box(data)).unwrap());
+        })
+    });
+
+    speed.finish();
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
+struct StructU16 {
+    height: u16,
+}
+
+fn criterion_struct_u16(c: &mut Criterion) {
+    let data: &str = r#" { "height" : 190 } "#;
+
+    let mut speed = c.benchmark_group("u16");
+
+    speed.bench_function("rigid::from_json", |b| {
+        b.iter(|| {
+            black_box(StructU16::from_json(black_box(data)).unwrap());
+        })
+    });
+
+    speed.bench_function("serde_json::from_str", |b| {
+        b.iter(|| {
+            black_box(serde_json::from_str::<StructU16>(black_box(data)).unwrap());
+        })
+    });
+
+    speed.finish();
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
+struct StructString {
+    height: String,
+}
+
+fn criterion_struct_string(c: &mut Criterion) {
+    let data: &str = r#" { "height" : "190" } "#;
+
+    let mut speed = c.benchmark_group("string");
+
+    speed.bench_function("rigid::from_json", |b| {
+        b.iter(|| {
+            black_box(StructString::from_json(black_box(data)).unwrap());
+        })
+    });
+
+    speed.bench_function("serde_json::from_str", |b| {
+        b.iter(|| {
+            black_box(serde_json::from_str::<StructString>(black_box(data)).unwrap());
+        })
+    });
+
+    speed.finish();
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
 struct Person {
     height: u8,
     birth_year: u16,
@@ -8,17 +108,11 @@ struct Person {
     alive: bool,
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn criterion_person(c: &mut Criterion) {
     let data: &str =
         r#" { "height": 187 , "birth_year": 1992 , "name": "aymeric" , "alive" : true } "#;
 
-    let mut speed = c.benchmark_group("speed");
-
-    //speed.bench_function("baseline", |b| {
-    //b.iter(|| {
-    //black_box(black_box(data));
-    //})
-    //});
+    let mut speed = c.benchmark_group("person");
 
     speed.bench_function("rigid::from_json", |b| {
         b.iter(|| {
@@ -32,35 +126,15 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    //speed.bench_function("json::parse", |b| {
-    //b.iter(|| {
-    //black_box(json::parse(black_box(data)).unwrap());
-    //})
-    //});
-
-    //speed.bench_function("simd_json::serde::from_slice", |b| {
-    //let mut data_mut = data.as_bytes().to_vec();
-    //b.iter(|| {
-    //black_box(simd_json::serde::from_slice::<Person>(black_box(&mut data_mut)).unwrap());
-    //})
-    //});
-
-    //speed.bench_function("simd_json::to_borrowed_value", |b| {
-    //let mut data_mut = data.as_bytes().to_vec();
-    //b.iter(|| {
-    //black_box(simd_json::to_borrowed_value(black_box(&mut data_mut)).unwrap());
-    //})
-    //});
-
-    //speed.bench_function("simd_json::to_owned_value", |b| {
-    //let mut data_mut = data.as_bytes().to_vec();
-    //b.iter(|| {
-    //black_box(simd_json::to_owned_value(black_box(&mut data_mut)).unwrap());
-    //})
-    //});
-
     speed.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    benches,
+    criterion_struct_bool,
+    criterion_struct_u8,
+    criterion_struct_u16,
+    criterion_struct_string,
+    criterion_person
+);
 criterion_main!(benches);
