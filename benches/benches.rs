225274d0285a459a9e9d3af45b4cff3f22970f9e
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-10-17 16:32:54 +0200

    add one-field tuple structs bench

--- benches/benches.rs
+++ benches/benches.rs
@@ -3,6 +3,26 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 #[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
 struct StringOnly(String);
 
+fn criterion_struct_one_field_tuple(c: &mut Criterion) {
+    let data: &str = r#""foobar""#;
+
+    let mut speed = c.benchmark_group("one_field_tuple");
+
+    speed.bench_function("rigid::from_json", |b| {
+        b.iter(|| {
+            black_box(StringOnly::from_json(black_box(data)).unwrap());
+        })
+    });
+
+    speed.bench_function("serde_json::from_str", |b| {
+        b.iter(|| {
+            black_box(serde_json::from_str::<StringOnly>(black_box(data)).unwrap());
+        })
+    });
+
+    speed.finish();
+}
+
 #[derive(Debug, PartialEq, serde::Deserialize, rigid::FromJSON)]
 struct StructEmpty {}
 
@@ -157,6 +177,7 @@ fn criterion_person(c: &mut Criterion) {
 
 criterion_group!(
     benches,
+    criterion_struct_one_field_tuple,
     criterion_struct_empty_many_spaces,
     criterion_struct_bool,
     criterion_struct_u8,
